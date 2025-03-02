451d7131d8661b8f2f981b1e3d710f2fe6c6e839
Aman Kumar <aman.kumar@geeksforgeeks.org>
2025-03-02 19:32:50 +0530

    add:WE-4

---
+++ WE-4/script.js
@@ -0,0 +1,19 @@
+// var,let,const
+
+// 1. Javascript is a scripting language which makes UI dynamic
+// 2. Js is single threaded and synchronous language
+
+// console.log(1);
+// console.log(2);
+// console.lo(3);
+// console.log(4);
+// console.log(5);
+
+// 3. js is loosely typed language
+
+// var a = 10
+// console.log(a);
+// a = 'hello'
+// console.log(a);
+// a = true
+// console.log(a);
