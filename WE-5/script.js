c1110591fea5177e271ed13914d24a43bbb12af1
Aman Kumar <aman.kumar@geeksforgeeks.org>
2025-03-08 19:20:58 +0530

    WE-5

---
+++ WE-5/script.js
@@ -0,0 +1,73 @@
+// var a //variable declaration
+
+// var b = 10 //variable initialization
+
+// function abc(){
+
+// }
+
+//function definition
+
+// var a = 10
+// console.log(typeof a);
+
+
+// a = 'hello'
+// console.log(typeof a);
+
+
+// a = true
+// console.log(typeof a);
+
+// var a = null
+// console.log(a);
+// console.log(b);
+
+// var obj = {
+//     name:'xyz',
+//     age:23,
+//     isStudent:true
+// }
+// console.log( obj);
+
+// var arr = [223,'bvcjdbvj',false]
+
+// console.log(arr);
+
+// var a = 100
+// var b = '10*10'
+
+// console.log(b-a);
+// console.log(a+b);
+
+// console.log(isNaN(a));
+// console.log(isNaN(b));
+
+// console.log(a==b*10);
+// console.log(a===b);
+
+// console.log('33'+3); //333  error
+// console.log('33'-3);//30
+// console.log(33-'3');//30
+// console.log(33+'3');//333
+// console.log('33'-'3');
+
+// var a = 10
+// console.log(typeof typeof a);
+
+
+// const a =10
+// a = 20
+
+// console.log(a);
+// const a = 10
+
+// let a = 10
+// {
+
+//     a = 20
+// }
+// console.log(a);
+
+let a = 'nvkdnvj'
+var a = 10
